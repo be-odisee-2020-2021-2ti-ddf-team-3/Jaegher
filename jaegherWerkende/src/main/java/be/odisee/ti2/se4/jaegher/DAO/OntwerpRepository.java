b202b3d3874d3a179e8b43e0f44f5e449d65cfae
EfremGokinan <55692736+EfremGokinan@users.noreply.github.com>
2021-04-08 11:51:12 +0200

    Update project

--- jaegherWerkende/src/main/java/be/odisee/ti2/se4/jaegher/DAO/OntwerpRepository.java
+++
@@ -1,7 +0,0 @@
-package be.odisee.ti2.se4.jaegher.DAO;
-
-import be.odisee.ti2.se4.jaegher.domain.Klant;
-import org.springframework.data.repository.CrudRepository;
-
-public interface OntwerpRepository extends CrudRepository<Klant, Long> {
-}
