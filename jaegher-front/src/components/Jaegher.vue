057bbeb25b041a4134372e418f1fe18f56cd8f64
BartMeeremans <55691474+BartMeeremans@users.noreply.github.com>
2021-04-29 17:42:33 +0200

    Jaegher frontend

---
+++ jaegher-front/src/components/Jaegher.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="jaegher">
+        <form @submit.prevent="getIdToLookup">
+            <input type="text" placeholder="id?" v-model="idToLookup" name="idToLookup">
+        </form>
+
+        <br/>
+
+        <div v-if=" info == 'error' ">
+            Unfortunately, something went wrong<br/><br/>
+        </div>
+        <div v-else  >
+            We have read the <strong>entry with id </strong> {{ info.id }} from the backend:
+            <ul>
+                <li><strong>User:</strong>
+                    {{ info.naam }}
+                    {{ info.achternaam }}
+                </li>
+            </ul>
+        </div>
+        <div v-if= " info == 'error' " >
+            <strong>Error: </strong> {{ error }}
+        </div>
+    </div>
+</template>
+
+<script>
+    import * as axios from "axios";
+
+    export default {
+        name: "Jaegher",
+        data () {
+            return {
+                idToLookup: '',
+                theUrl: '',
+                info: null,
+                status: null,
+                error: null
+            }
+        },
+        mounted () {
+            this.idToLookup = '1'
+            this.getIdToLookup()
+        },
+        methods: {
+            getIdToLookup() {
+                this.theUrl = 'http://localhost:8080/jaegherrest/' + this.idToLookup
+                axios
+                    .get(this.theUrl)
+                    .then(response => (this.info = response.data,
+                        this.status = response.status) )
+                    .catch(error => (this.info = "error",
+                        this.status = error.response.status,
+                        this.error = error.response.data.message ) )
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    h3 {
+        margin: 40px 0 0;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+</style>
