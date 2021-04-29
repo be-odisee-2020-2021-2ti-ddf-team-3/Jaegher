ad94c9c5b2d7e11abedc5ab130794e82313ad4d4
BartMeeremans <55691474+BartMeeremans@users.noreply.github.com>
2021-04-29 17:18:46 +0200

    update

---
+++ FrontEnd/src/main/java/be/odisee/jaegherfrontend/JaegherFrontEndApplication.java
@@ -0,0 +1,13 @@
+package be.odisee.jaegherfrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaegherFrontEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaegherFrontEndApplication.class, args);
+    }
+
+}
