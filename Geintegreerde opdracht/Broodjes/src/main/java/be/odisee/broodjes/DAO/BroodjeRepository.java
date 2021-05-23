f99d435211855a60c01f84ab5ce7692c12550386
BartMeeremans <55691474+BartMeeremans@users.noreply.github.com>
2021-05-23 15:05:17 +0200

    1

--- Geintegreerde opdracht/Broodjes/src/main/java/be/odisee/broodjes/DAO/BroodjeRepository.java
+++ Geintegreerde opdracht/Broodjes/src/main/java/be/odisee/broodjes/DAO/BroodjeRepository.java
@@ -1,8 +1,9 @@
 package be.odisee.broodjes.DAO;
 
 import be.odisee.broodjes.domain.Broodje;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface BroodjeRepository extends CrudRepository<Broodje, Long> {
+public interface BroodjeRepository extends JpaRepository<Broodje, Long> {
     public Broodje findById(long id);
 }
